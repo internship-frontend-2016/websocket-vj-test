1e43367faa2b257f830b9ef0dc1a82afe5ab739b
TEAM-LAB\tazawamichiko <tmchisuper777@gmail.com>
2016-08-30 17:34:28 +0900

    機能を関数化した

--- src/js/vj-controller.js
+++ src/js/vj-controller.js
@@ -1,23 +1,21 @@
-var canvas;
-var submitCanvas;
-window.onload=function(){
-	var socket=io();
-	canvas=document.getElementById("myCanvas");
-	submitCanvas=document.getElementById("submitCanvas");
-	var subc=submitCanvas.getContext("2d");
-	var c=canvas.getContext("2d");
 
+window.onload=function(){
+	var canvas=document.getElementById("myCanvas");
 	var w=316;
 	var h=316;
+	initPaintCanvas(w,h,canvas);
+	init();
+	initControllerPad(w,h,canvas)
+}
+
+//絵を描く部分
+function initPaintCanvas(_w,_h,_canvas){
+	var c=_canvas.getContext("2d");
 	var drawing=false;
 	var oldPos;
 
-	canvas.width=w;
-	canvas.height=h;
-
-	//送信する側のキャンバス
-	submitCanvas.width=w;
-	submitCanvas.height=h;
+	_canvas.width=_w;
+	_canvas.height=_h;
 
 	c.strokeStyle="black";
 	c.lineWidth=5;
@@ -25,27 +23,26 @@ window.onload=function(){
 	c.lineCap="round";
 
 	//タップ開始時に、絵を描く準備をする
-	canvas.addEventListener("touchstart",function(e){
+	_canvas.addEventListener("touchstart",function(e){
 		drawing=true;
-		oldPos=getPosT(e,canvas);
+		oldPos=getPosT(e,_canvas);
 	},false);
 
 	//タップ終了時に、絵を描く後処理を行う
-	canvas.addEventListener("touched",function(){
+	_canvas.addEventListener("touched",function(){
 		drawing=false
 	},false);
 
 	//gestureイベント（2本指以上触ると発生する）
 	//終了時にも絵を描く後処理を行う
-	canvas.addEventListener("gestureend",function(){
-		console.log("mouseout");
+	_canvas.addEventListener("gestureend",function(){
 		drawing=false;
 	},false);
 
 	//実際に絵を書く処理
 	//前回保存した位置から現在の位置に線を引く
-	canvas.addEventListener("touchmove",function(e){
-	var pos = getPosT(e,canvas);
+	_canvas.addEventListener("touchmove",function(e){
+	var pos = getPosT(e,_canvas);
 		if(drawing){
 			c.beginPath();
 			c.moveTo(oldPos.x,oldPos.y);
@@ -65,10 +62,14 @@ window.onload=function(){
 	setEventLineWidth("middle",c);
 	setEventLineWidth("large",c);
 
+	//削除ボタン
 	document.getElementById("delete_button").addEventListener("click",function(){
-		c.clearRect(0,0,$(canvas).width(),$(canvas).height());		
+		c.clearRect(0,0,$(_canvas).width(),$(_canvas).height());		
 	},false);
 
+}
+//禁止事項など
+function init(){
 	//ドキュメントのタッチイベントの初期化
 	document.addEventListener("touchstart",stopDefault,false);
 	document.addEventListener("touchmove",stopDefault,false);
@@ -76,72 +77,52 @@ window.onload=function(){
 	//ドキュメントのジェスチャーイベントの初期化
 	document.addEventListener("gesturestart",stopDefault,false);
 	document.addEventListener("gesturechange",stopDefault,false);
-	//document.addEventListener("gestureend",stopDefault,false);
+	//document.addEventListener("gestureend",stopDefault,false);	
+}
+//送信ボタン
+function initControllerPad(_w,_h,_canvas){
+	var socket=io();
+	var submitCanvas=document.getElementById("submitCanvas");
+	var subc=submitCanvas.getContext("2d");
 
-
-
+	//送信する側のキャンバス
+	submitCanvas.width=_w;
+	submitCanvas.height=_h;
 	//画像をサーバーに送信する処理
-	//var toImage=document.getElementById("toImage_AppearBack");
-	document.getElementById("toImage_AppearBack").addEventListener("click",function(){
-		var image=createbase64();
-		console.log(image);
-		socket.emit("pushImageFromClient",{
-			imgdata:image,
-			AppearBack:true
-		});
-	},false);
-	document.getElementById("toImage_DropFromUp").addEventListener("click",function(){
-		var image=createbase64();
-		console.log(image);
-		socket.emit("pushImageFromClient",{
-			imgdata:image,
-			DropFromUp:true
-		});
-	},false);
-
 	var subPos;
 	var normalPosX,normalPosY;
 	var image;
-	var sentFlag;
 	var sendingTimer;
 	/*-------------画像を送信する--------------*/
 
 	submitCanvas.addEventListener("touchstart",function(e){
-		sentFlag=true;
 		subPos=getPosT(e,submitCanvas);
-		normalPosX=(subPos.x*2-w)/w;
-		normalPosY=-((subPos.y*2-h)/h);
-		image=createbase64();
-	sendingTimer=setInterval(sentdata,100);
+		normalPosX=(subPos.x*2-_w)/_w;
+		normalPosY=-((subPos.y*2-_h)/_h);
+		image=createbase64(_canvas);
+		sendingTimer=setInterval(sentdata,100);
 	},false);
 
 	submitCanvas.addEventListener("touchmove",function(e){
-		//sentFlag=true;
 		subPos=getPosT(e,submitCanvas);
-		normalPosX=(subPos.x*2-w)/w;
-		normalPosY=-((subPos.y*2-h)/h);
-		image=createbase64();
+		normalPosX=(subPos.x*2-_w)/_w;
+		normalPosY=-((subPos.y*2-_h)/_h);
+		image=createbase64(_canvas);
 	},false);
 	submitCanvas.addEventListener("touchend",function(){
-		sentFlag=false;
-		console.log("touchend");
 		clearInterval(sendingTimer);
 	},false);
 
-	function sentdata(){
-		console.log("sentdata_function");
-		
+	function sentdata(){		
 		socket.emit("pushImageFromClient",{
 			imgdata:image,
 			x:normalPosX,
 			y:normalPosY
 		});
-				
 	}
-
 }
-function createbase64(){
-	var data=canvas.toDataURL("image/png");
+function createbase64(_canvas){
+	var data=_canvas.toDataURL("image/png");
 	var imageContents=document.getElementById("imageContents");
 	imageContents.innerHTML="<img src='"+data+"'>";	
 	return data;
@@ -155,16 +136,13 @@ function getPosT(_e,_canvas){
 	return {x:mouseX,y:mouseY};
 }
 function setEventButtonColor(_id,_c){
-	console.log(_id);
 	document.getElementById(_id).addEventListener("click",function(){
-		console.log(_id);
 		_c.strokeStyle=_id;
 	},false);
 }
 
 function setEventLineWidth(_id,_c){
 	document.getElementById(_id).addEventListener("click",function(){
-		console.log(_id+"_click");
 		var strokewidth;
 		switch(_id){
 			case "small":
@@ -179,7 +157,6 @@ function setEventLineWidth(_id,_c){
 			default:
 				break;
 		}
-		console.log(strokewidth);
 		_c.lineWidth=strokewidth;
 	},false);
 }
@@ -187,7 +164,6 @@ function setEventLineWidth(_id,_c){
 //ブラウザのデフォルト動作を禁止する
 function stopDefault(e){
 	if(e.touches[0].target.tagName.toLowerCase() == "li"){
-		//console.log(e.touches[0].target.tagName.toLowerCase());
 		return;
 	}
 	if(e.touches[0].target.tagName.toLowerCase()=="div"){
